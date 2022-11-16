fe38c42ee7c814d77b44d354e3a636681953b848
l3bao84 <l3ducb4008@gmail.com>
2022-11-16 20:03:55 +0700

    Add project files.

---
+++ demo2/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace demo2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool checkAge(int year)
+        {
+            bool ageChecked = true;
+            if(int.Parse(DateTime.Today.Year.ToString()) - year < 18)
+            {
+                MessageBox.Show("Tuổi của nv phải >= 18!");
+                ageChecked = false;
+            }
+            return ageChecked;
+        }
+
+        public bool checkTextInput(string gioitinh)
+        {
+            bool textInputChecked = true;
+            if(this.txtManv.Text == "" && this.txtName.Text == "" && this.txtBirthDay.Text == "" && 
+                gioitinh == "" && this.cbApartment.Text == "" && this.txtHS.Text == "")
+            {
+                MessageBox.Show("Phải nhập dữ liệu cho tất cả các trường!");
+                textInputChecked = false;
+            }
+            return textInputChecked;
+        }
+
+        private void btn_Input(object sender, RoutedEventArgs e)
+        {
+
+            // get data from form
+            string manv = txtManv.Text;
+            string hoten = this.txtName.Text;
+            string handleString = this.txtBirthDay.Text;
+            string gioitinh = "";
+            if(this.rdMale.IsChecked == true)
+            {
+                gioitinh = "Nam";
+            } else if(this.rdFemale.IsChecked == true)
+            {
+                gioitinh = "Nữ";
+            }
+            string phongban = this.cbApartment.Text;
+
+
+            // Check requirement
+            if(checkTextInput(gioitinh))
+            {
+                String[] array = handleString.Split("-");
+                DateTime ngaysinh = DateTime.Parse(array[2] + "-" + array[1] + "-" + array[0]);
+                if (checkAge(int.Parse(array[2]))) {
+                    double hs = Convert.ToDouble(this.txtHS.Text);
+                    if (hs <= 0)
+                    {
+                        MessageBox.Show("Hệ số lương phải > 0");
+                    }else
+                    {
+                        NhanVien nv = new NhanVien(manv, hoten, ngaysinh, gioitinh, phongban, hs);
+                        this.dtGrid.Items.Add(nv);
+                    }
+                }
+            }
+        }
+
+        private void showCurrentTime(object sender, RoutedEventArgs e)
+        {
+            DateTime currentTime = DateTime.Today;
+            this.txtBirthDay.Text = currentTime.ToString("dd-MM-yyyy");
+        }
+    }
+}
---
+++ demo2/NhanVien.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo2
+{
+    internal class NhanVien
+    {
+        private string manv { get; set; }
+        private string hoten { get; set; }
+        private DateTime ngaysinh { get; set; }
+        private string gioitinh { get; set; }
+        private string phongban { get; set; }
+        private double hsLuong { get; set; }
+        public NhanVien()
+        {
+
+        }
+        public NhanVien(string manv, string hoten, DateTime ngaysinh, string gioitinh, string phongban, double hsLuong)
+        {
+            this.manv = manv;
+            this.hoten = hoten;
+            this.ngaysinh = ngaysinh;
+            this.gioitinh = gioitinh;
+            this.phongban = phongban;
+            this.hsLuong = hsLuong;
+        }
+
+        public int tinhTuoi(int x)
+        {
+            return this.ngaysinh.Year - x;
+        }
+    }
+}
